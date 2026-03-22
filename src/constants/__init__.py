b858b91575c89c9f08673ef6d25df9608e28af69
DAIZYGUPTA <guptadaizy2495@gmail.com>
2026-03-22 18:11:21 +0530

    created project structure

---
+++ src/constants/__init__.py
@@ -0,0 +1,8 @@
+# Environment variable key for MongoDB connection
+MONGODB_URL_KEY: str = "MONGO_DB_URL"
+
+# Database name
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+# Session key for storing product name
+SESSION_PRODUCT_KEY: str = "product_name"
